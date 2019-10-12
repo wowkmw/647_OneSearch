ca37eabd9426314362b7a1e2fd54f6d572050ca4
Pengji Zhu <n10065971@qut.edu.au>
2019-10-12 16:22:27 +1000

    Comment Bill.

--- OneSearch/Program.cs
+++ OneSearch/Program.cs
@@ -17,6 +17,7 @@ namespace OneSearch
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // This is Bill
         }
     }
 }
